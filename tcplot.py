033df0365baf2567d79eaf2c9e18ec0eb6ebdbfd
Wei-Hsun Lin <wei.xun.lin@gmail.com>
2018-02-16 13:35:28 +0800

    add grouping box color

--- tcplot.py
+++ tcplot.py
@@ -11,7 +11,7 @@ ax_size_x_ratio = 0.925
 ax_size_y_ratio = 0.5
 
 fig_size_x_in = 10
-fig_size_y_in = 2.5
+fig_size_y_in = 3.1
 dot_radius_in = 0.1
 
 igroup_indi_offset_y_in = 0.6
@@ -31,15 +31,17 @@ def tcbase(df, ax, para):
     ax_height = para['lim_y_max'] - para['lim_y_min']
     ax_width = para['lim_x_max'] - para['lim_x_min']
 
-    ax.plot(df.ORD, df.value, linewidth=2.0, zorder=1)
+    ax.plot(df.ORD, df.value, 'k', linewidth=2.0, zorder=1)
     plt.xticks(df.ORD, df.id.tolist(), rotation=-90)
-    ax.tick_params(labelsize=6)
+    ax.tick_params('x', labelsize=6)
+    ax.tick_params('y', labelsize=12)
 
     ax.set_xlim([lim_x_min, lim_x_max])
     ax.set_ylim([lim_y_min, lim_y_max])
 
     dot_radius_x = (dot_radius_in / fig_size_y_in *
                     ax_size_y_ratio) * ax_width
+
     for x, y in zip(df.ORD, df.value):
         ax.add_patch(
             patches.Ellipse(
@@ -49,7 +51,7 @@ def tcbase(df, ax, para):
                                 (fig_size_x_in * ax_size_x_ratio / ax_width)),
                 clip_on=False,
                 zorder=200,
-                color='r'
+                color='tab:orange'
             )
         )
         ax.add_patch(
@@ -92,9 +94,29 @@ def add_group_sep(df, ax, group_id, level, para, mode='line'):
                 )
             )
             ax.text(s0 / 2 + s1 / 2, (-igroup_indi_offset_y_in - igroup_indi_height_y_in * (level + 0.5) - igroup_indi_sep_y_in / 2) /
-                    (fig_size_y_in * ax_size_y_ratio / ax_height), gr, horizontalalignment='center', verticalalignment='center')
+                    (fig_size_y_in * ax_size_y_ratio / ax_height), gr, ha='center', va='center')
     elif mode == 'box':
-        for s0, s1,gr in zip(sep[1:], sep[:-1],group):
+        if 'color_table' not in para.keys():
+            para['color_table'] = dict()
+        color_idx = 0
+        for s0, s1, gr in zip(sep[1:], sep[:-1], group):
+            if gr not in para['color_table'].keys():
+                while 'C' + str(color_idx) in para['color_table'].values():
+                    color_idx += 1
+                para['color_table'][gr] = 'C' + str(color_idx)
+            ax.add_patch(
+                patches.Rectangle(
+                    (s0, (-igroup_indi_offset_y_in - igroup_indi_height_y_in * (level + 1)) /
+                        (fig_size_y_in * ax_size_y_ratio / ax_height)),
+                    s1 - s0,
+                    igroup_indi_height_y_in /
+                    (fig_size_y_in * ax_size_y_ratio / ax_height),
+                    color=para['color_table'][gr],
+                    clip_on=False,
+                    zorder=200,
+                    linewidth=0.75,
+                )
+            )
             ax.add_patch(
                 patches.Rectangle(
                     (s0, (-igroup_indi_offset_y_in - igroup_indi_height_y_in * (level + 1)) /
@@ -109,8 +131,15 @@ def add_group_sep(df, ax, group_id, level, para, mode='line'):
                 )
             )
             ax.text(s0 / 2 + s1 / 2, (-igroup_indi_offset_y_in - igroup_indi_height_y_in * (level + 0.5) - igroup_indi_sep_y_in / 2) /
-                    (fig_size_y_in * ax_size_y_ratio / ax_height), gr, horizontalalignment='center', verticalalignment='center')
+                    (fig_size_y_in * ax_size_y_ratio / ax_height), gr, ha='center', va='center', zorder=201)
 
+    return
+
+
+def add_spec_line(df, ax, spec, label, para, col='r'):
+    ax.plot([para['lim_x_min'], para['lim_x_max']],
+            [spec, spec], '--', linewidth=1.0, zorder=1,color=col)
+    ax.text(0, spec, label, ha='left', va='bottom',color=col)
     return
 
 
@@ -173,10 +202,13 @@ def main(dic_arg):
     # plot base plot
     tcbase(df, ax, dic_plot_para)
 
+    add_spec_line(df, ax, 3, 'GB', dic_plot_para)
+
     # add grouping indicators
+    dic_plot_para['color_table'] = {}
     if (dic_arg['group']) != None:
         for level, group_id in enumerate(dic_arg['group']):
-            add_group_sep(df, ax, group_id, level,
+            add_group_sep(df, ax, group_id, len(dic_arg['group']) - level - 1,
                           mode=dic_arg['groupstyle'][0], para=dic_plot_para)
 
     # save figure to file
@@ -202,7 +234,7 @@ if __name__ == '__main__':
                         nargs=1, type=int, help="data y upper limit")
     parser.add_argument('-g', '--group', metavar='GROUPS',
                         nargs='+', help="grouping condition")
-    parser.add_argument('--groupstyle', metavar='STYLE', default='line', type=str,
+    parser.add_argument('--groupstyle', metavar='STYLE', default=['line'], type=str,
                         nargs=1, help="grouping style (line or box)")
 
     args = parser.parse_args()
